28d4bffa711b08e0ceb9ff1fac2076d0d2c97844
RASanchezB <rasanchez@unitec.edu>
2017-12-15 17:35:31 -0600

    Crea el archivo

--- Main.cpp
+++ Main.cpp
@@ -17,21 +17,22 @@ int main(){
 		<<"3)Salir"<<endl
 		<<"Ingrese la opcion que desee"<<endl;
 		cin>>opcion;
-		cout<<endl;
-		cout<<opcion<<endl;
-		cout<<endl;
 		switch (opcion) {
 			case 1:{
-				string Mensaje;
+				cout<<endl;
+				//Crear y agregar un mensaje al archivo
+				string Mensaje = "";//Mensaje que se agregara al archivo
+				string Agregar = "";//Para poder agregar mas de una linea
 				cout<<"Ingrese un titulo para el archivo (porfavor excluir el .txt)"<<endl;
-				cin>>archive;
+				cin>>archive;//nombre del archivo a subir
 				archive+=".txt";
 				int opcion2 = 1;
 				while(opcion2 == 1){
 					cout<<"Ingrese lo que va a escribir en el archivo"<<endl;
 					cin.ignore();
-					getline(cin,Mensaje);
-					cout<<"El mensaje dice: "<<Mensaje<<endl;
+					getline(cin,Agregar);
+					Mensaje+=Agregar;
+					cout<<endl;
 					cout<<"--Menu secundario--"<<endl
 					<<"1)Escribir otra linea"<<endl
 					<<"2)Terminar"<<endl
@@ -44,7 +45,42 @@ int main(){
 				salida<<archivo;
 				salida.close();
 			}break;
-			case 2:{}break;
+			case 2:{
+				cout<<endl;
+				//Editar un archivo ya existente
+				string Mensaje;//Mensaje que se agregara al archivo
+				cout<<"Ingrese un titulo para el archivo a Editar(porfavor excluir el .txt)"<<endl;
+				cin>>archive;//nombre del archivo a editar
+				archive+=".txt";
+				string linea;
+				int NumLineas=0;
+				ifstream file (archive.c_str());
+				if(file.is_open()){
+					while(getline(file,linea)){
+						NumLineas++;
+					}
+					file.close();
+				}
+				ArrayStack* TextoA = new ArrayStack(NumLineas);
+				if (file.is_open()){
+					cout<<"El contenido del archivo dice: "<<endl;
+					cout<<endl;
+					while (getline(file,linea)){
+						cout<<linea<<'\n';
+						TextoA->push(linea);
+					}
+					while (!TextoA->isEmpty()) {
+						cout<<TextoA->pop()<<"";
+					}
+					cout<<endl;
+					delete TextoA;
+					file.close();
+					cout<<endl;
+
+				}else{
+					cout<<"No existe ese archivo"<<endl;
+				}
+			}break;
 			case 3:{
 				cout<<"Saliendo"<<endl;
 			}break;
